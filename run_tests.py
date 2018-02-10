5fcce1b55f2d09afb0dc0343d5d5dc50c8380f79
Bertrand Bordage <bordage.bertrand@gmail.com>
2018-02-10 03:25:55 +0100

    Final attempt at fixing Travis.
    Or maybe not.

--- run_tests.py
+++ run_tests.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tests.settings')
     django.setup()
     from django.test.runner import DiscoverRunner
-    test_runner = DiscoverRunner(verbosity=2)
+    test_runner = DiscoverRunner(verbosity=2, interactive=False)
     failures = test_runner.run_tests(['tests.base'])
     if failures:
         sys.exit(failures)
