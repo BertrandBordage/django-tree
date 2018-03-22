e7011ce021fa92d44bc4ed9d49f981708e28444b
Bertrand Bordage <bordage.bertrand@gmail.com>
2018-03-22 03:35:00 +0100

    Fixes Level transform in Django 1.8.

--- tree/transforms.py
+++ tree/transforms.py
@@ -1,9 +1,16 @@
+import django
 from django.db.models import IntegerField, Transform
 
 
 class Level(Transform):
     lookup_name = 'level'
     function = 'length'
+
+    if django.VERSION < (1, 9):
+        def as_sql(self, compiler, connection):
+            lhs, params = compiler.compile(self.lhs)
+            return self.template % {'function': self.function,
+                                    'expressions': lhs}, params
 
     @property
     def template(self):
