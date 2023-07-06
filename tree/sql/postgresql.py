3e79ae4e6d067c4d30191eea28140007479adce8
Bertrand Bordage <bordage.bertrand@gmail.com>
2023-07-06 16:46:02 +0200

    Fixes an implicit type casting not happening in slightly older versions of PostgreSQL.

--- tree/sql/postgresql.py
+++ tree/sql/postgresql.py
@@ -79,7 +79,7 @@ def get_update_paths_function_creation(
             ) UNION ALL (
                 SELECT
                     t2.{pk},
-                    t1.path || row_number() OVER (
+                    t1.path || row_number()::decimal[] OVER (
                         PARTITION BY t1.pk ORDER BY {sql_t2_order_by}
                     ) - 1
                 FROM generate_paths AS t1
